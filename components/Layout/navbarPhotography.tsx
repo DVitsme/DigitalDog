d52145b3fac69355ef9c22beb182d5d08b3c3dfa
DVitsme <derrick.valentine@gmail.com>
2023-02-16 09:22:31 -0500

    add margin remove console logs

--- components/Layout/navbarPhotography.tsx
+++ components/Layout/navbarPhotography.tsx
@@ -21,7 +21,7 @@ const NavBarPhotography = () => {
           viewBox="0 0 1155 678"
         ></svg>
       </div>
-      <div className="px-6 pt-6 lg:px-8">
+      <div className="px-6 pt-6 mb-8 lg:px-8">
         <nav className="flex items-center justify-between" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
@@ -38,16 +38,11 @@ const NavBarPhotography = () => {
               type="button"
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
               onClick={() => {
-                console.log('I was clicked');
                 setMobileMenuOpen(true);
               }}
             >
               <span className="sr-only">Open main menu</span>
-              <HiBars3BottomLeft
-                className="h-6 w-6"
-                aria-hidden="true"
-                onClick={() => console.log('I was clicked')}
-              />
+              <HiBars3BottomLeft className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
@@ -71,10 +66,7 @@ const NavBarPhotography = () => {
           </div>
         </nav>
         <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <Dialog.Panel
-            focus="true"
-            className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden"
-          >
+          <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
